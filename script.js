82eebb9401bbc8ef769c25de1e5d856c5bfae77a
Nadhorion <67388006+Nadhorion@users.noreply.github.com>
2026-03-03 21:10:35 -0500

    Add functionality to new grid button
    Will allow user to choose custom grid size

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function makeGrid(length, width) {
 
-    const loopGuard = 0;
+    removeOldGrid();    
     const grid = document.querySelector('#grid');
+
 
     for (let i = 0; i < length; i++) {
 
@@ -23,4 +24,33 @@ function makeGrid(length, width) {
     }
 }
 
-makeGrid(50, 50);
+function removeOldGrid() {
+
+    document.querySelector('#grid').remove()
+    const newGridDiv = document.createElement('div');
+    newGridDiv.id = 'grid';
+
+    const body = document.querySelector('body');
+    body.appendChild(newGridDiv)
+}
+
+function promptGridSize() {
+
+    let gridSize = +prompt(
+        'Enter a grid size (ex. 16 makes 16*16 grid. MAX 100)', 16
+    )
+
+    if (gridSize 
+        && Math.floor(gridSize) === gridSize 
+        && gridSize <= 100
+        && gridSize > 0) {
+            
+        makeGrid(gridSize, gridSize)
+
+    } else {
+        alert('Invalid entry. Grid size must be a whole number between 1 and 100.')
+    }
+}
+
+const newGridButton = document.querySelector('button');
+newGridButton.addEventListener('click', promptGridSize);
